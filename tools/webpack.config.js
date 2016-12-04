379e39fde161a8c01436ac7675742ca8cf35dc49
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2016-12-04 19:23:18 +0800

    remove unuseful config

--- tools/webpack.config.js
+++ tools/webpack.config.js
@@ -2,15 +2,15 @@
 
 var webpack = require('webpack'),
 	config = require('./config'),
-	isProduction = (config.env === '__PROD__'),
-	gutil = require('gulp-util');
+	isProduction = (config.env === '__PROD__');
+	// gutil = require('gulp-util');
 
 
 if (isProduction) {
 	var compiler = webpack(require('./webpack.pub'));
 	compiler.run(function(err, stats) {
 	    if (!err) {
-	    	gutil.log('[webpack:pub]', stats.toString({
+	    	console.log(stats.toString({
 	            chunks: false, // Makes the build much quieter
 	            colors: true
 	        }));
