b238a75cdb2319b0a9c8501bfc628d91b9370c95
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2016-12-23 00:22:00 +0800

    improve structure

---
+++ .steamer/steamer-react.js
@@ -0,0 +1,22 @@
+module.exports = [
+	{
+        type: 'input',
+        name: 'webserver',
+        message: 'html url(//localhost:9000/)'
+    },
+    {
+        type: 'input',
+        name: 'cdn',
+        message: 'cdn url(//localhost:8000/)'
+    },
+	{
+        type: 'input',
+        name: 'port',
+        message: 'development server port(9000)'
+    },
+    {
+        type: 'input',
+        name: 'route',
+        message: 'development server directory(/news/)'
+    }
+];
