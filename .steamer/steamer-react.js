f3f0ce07b75abf1c2ac11f5f16eda8364625fb9e
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2016-12-24 23:00:10 +0800

    modify steamer config

--- .steamer/steamer-react.js
+++ .steamer/steamer-react.js
@@ -1,22 +1,24 @@
-module.exports = [
-	{
-        type: 'input',
-        name: 'webserver',
-        message: 'html url(//localhost:9000/)'
-    },
-    {
-        type: 'input',
-        name: 'cdn',
-        message: 'cdn url(//localhost:8000/)'
-    },
-	{
-        type: 'input',
-        name: 'port',
-        message: 'development server port(9000)'
-    },
-    {
-        type: 'input',
-        name: 'route',
-        message: 'development server directory(/news/)'
-    }
-];
+module.exports = {
+    options: [
+        {
+            type: 'input',
+            name: 'webserver',
+            message: 'html url(//localhost:9000/)'
+        },
+        {
+            type: 'input',
+            name: 'cdn',
+            message: 'cdn url(//localhost:8000/)'
+        },
+        {
+            type: 'input',
+            name: 'port',
+            message: 'development server port(9000)'
+        },
+        {
+            type: 'input',
+            name: 'route',
+            message: 'development server directory(/news/)'
+        }
+    ]
+};
