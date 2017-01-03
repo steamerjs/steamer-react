a89a69f0c4ee3de07a8917868283b37ba91b03d8
lcxfs1991@gmail.com <lcxfs1991@gmail.com>
2017-01-03 18:21:52 +0800

    improve log

--- tools/build.js
+++ tools/build.js
@@ -5,15 +5,32 @@ process.env.NODE_ENV = "__PROD__";
 var webpack = require('webpack'),
 	config = require('./config');
 
+    var fs = require('fs');
+
 var compiler = webpack(require('./webpack.prod'));
 compiler.run(function(err, stats) {
     if (!err) {
-    	console.log(stats.toString({
+        const jsonStats = stats.toJson();
+        // print asset stats
+        // fs.writeFileSync("stats.txt", JSON.stringify(jsonStats, " " , 4))
+        
+        console.log(stats.toString({
+            cached: true,
             chunks: false, // Makes the build much quieter
-            colors: true
+            colors: true,
+            children: false, // supress some plugin output
         }));
+
+        if (jsonStats.errors.length > 0) {
+            console.log('Webpack compiler encountered errors.')
+            console.log(jsonStats.errors.join('\n'))
+            // return reject(new Error('Webpack compiler encountered errors'))
+        } else if (jsonStats.warnings.length > 0) {
+            console.log('Webpack compiler encountered warnings.')
+            debug(jsonStats.warnings.join('\n'))
+        }
     }
     else {
-    	console.log(err);
+        console.log(err);
     }
 });
