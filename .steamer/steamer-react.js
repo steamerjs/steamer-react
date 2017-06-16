66bb1b572e18ed3d83e1f5cb487c523db0b9a497
heyli <heyli@tencent.com>
2017-06-16 16:28:27 +0800

    add style lint

--- .steamer/steamer-react.js
+++ .steamer/steamer-react.js
@@ -7,6 +7,7 @@ module.exports = {
         ".eslintrc.js",
         ".eslintignore",
         ".stylelintrc.js",
+        ".stylelintignore",
         "postcss.config.js",
         ".gitignore",
         ".babelrc",
--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,26 +1,4 @@
 module.exports = {
-	"extends": "stylelint-config-standard",
-	"rules": {
-		"indentation": null,
-		"comment-empty-line-before":null,
-	    "declaration-empty-line-before": [ "always", {
-	    	except: [
-	    		"first-nested",
-	    	],
-	      	ignore: [
-	      		"after-declaration",
-	        	"after-comment",
-	        	"inside-single-line-block",
-	      	],
-	    }],
-	    "rule-nested-empty-line-before": null,
-	    "no-missing-end-of-source-newline": null,
-	    "no-eol-whitespace": null,
-	    "no-empty-source": null,
-	    "media-feature-name-no-unknown": null,
-	    "max-empty-lines": null,
-	    "rule-non-nested-empty-line-before": null,
-	    "length-zero-no-unit": null,
-	    "function-whitespace-after": null
-	}
+	"extends": "@alloyteam/stylelint-config-standard",
+	"rules": {}
 }
