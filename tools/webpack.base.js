b79ed1d9d13539a35ebcdf49aa278132a11532ef
heyli <heyli@tencent.com>
2017-07-10 15:18:26 +0800

    update webpack config

--- tools/webpack.base.js
+++ tools/webpack.base.js
@@ -12,14 +12,14 @@ var config = require('../config/project'),
     configWebpackMerge = config.webpackMerge,
     configCustom = config.custom,
     isProduction = config.env === 'production',
-    isWindows = (os.type() === "Windows_NT");
+    isWindows = (os.type() === 'Windows_NT');
 
 var Clean = require('clean-webpack-plugin'),
-    CopyWebpackPlugin = require("copy-webpack-plugin-hash"),
+    CopyWebpackPlugin = require('copy-webpack-plugin-hash'),
     SpritesmithPlugin = require('webpack-spritesmith'),
     WebpackMd5Hash = require('webpack-md5-hash'),
     UglifyJsParallelPlugin = require('webpack-uglify-parallel'),
-    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
     NameAllModulesPlugin = require('name-all-modules-plugin');
 
 var baseConfig = {
@@ -28,8 +28,8 @@ var baseConfig = {
     output: {
         publicPath: isProduction ? configWebpack.cdn : configWebpack.webserver,
         path: isProduction ? path.join(configWebpack.path.dist, configWebpack.path.distCdn) : configWebpack.path.dev,
-        filename: configWebpack.chunkhashName + ".js",
-        chunkFilename: "chunk/" + configWebpack.chunkhashName + ".js",
+        filename: configWebpack.chunkhashName + '.js',
+        chunkFilename: 'chunk/' + configWebpack.chunkhashName + '.js'
     },
     module: {
         rules: [
@@ -37,37 +37,37 @@ var baseConfig = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 options: {
-                    cacheDirectory: './.cache/',
+                    cacheDirectory: './.cache/'
                 },
-                exclude: /node_modules/,
+                exclude: /node_modules/
             },
             { 
                 test: /\.jsx$/,
                 loader: 'babel-loader',
                 options: {
-                    "plugins": [
-                        ["transform-react-jsx", { "pragma": "h" }]
+                    'plugins': [
+                        ['transform-react-jsx', { 'pragma': 'h' }]
                     ],
-                    cacheDirectory: './.cache/',
+                    cacheDirectory: './.cache/'
                 },
-                exclude: /node_modules/,
+                exclude: /node_modules/
             },
             {
                 test: /\.ico$/,
-                loader: "url-loader",
+                loader: 'url-loader',
                 options: {
-                    name: "[name].[ext]"
-                },
-            },
-        ],
+                    name: '[name].[ext]'
+                }
+            }
+        ]
     },
     resolve: {
         modules: [
             configWebpack.path.src,
-            "node_modules",
-            path.join(configWebpack.path.src, "css/sprites")
+            'node_modules',
+            path.join(configWebpack.path.src, 'css/sprites')
         ],
-        extensions: [".js", ".jsx", ".css", ".scss", "sass", ".less", ".styl", ".png", ".jpg", ".jpeg", ".ico", ".ejs", ".pug", ".handlebars", "swf"],
+        extensions: ['.js', '.jsx', '.css', '.scss', 'sass', '.less', '.styl', '.png', '.jpg', '.jpeg', '.ico', '.ejs', '.pug', '.handlebars', 'swf'],
         alias: {}
     },
     plugins: [
@@ -75,18 +75,18 @@ var baseConfig = {
         new webpack.NamedModulesPlugin(),
         new NameAllModulesPlugin()
     ],
-    watch: isProduction ? false : true,
+    watch: !isProduction,
     devtool: isProduction ? configWebpack.sourceMap.production : configWebpack.sourceMap.development
 };
 
-/************* loaders 处理 *************/
+/** *********** loaders 处理 *************/
 // 样式loader
 var commonLoaders = [
     {
-        loader: "cache-loader",
+        loader: 'cache-loader',
         options: {
             // provide a cache directory where cache items should be stored
-            cacheDirectory: path.resolve(".cache")
+            cacheDirectory: path.resolve('.cache')
         }
     },
     {
@@ -94,7 +94,7 @@ var commonLoaders = [
         options: {
             localIdentName: '[name]-[local]-[hash:base64:5]',
             module: config.webpack.cssModule,
-            autoprefixer: true,
+            autoprefixer: true
         }
     },
     { 
@@ -117,28 +117,28 @@ var styleRules = {
         loader: ExtractTextPlugin.extract({
             fallback: 'style-loader', 
             use: merge([], commonLoaders).concat([{
-                loader: 'less-loader',
+                loader: 'less-loader'
             }])
-        }),
+        })
     },
     stylus: {
         test: /\.styl$/,
         loader: ExtractTextPlugin.extract({
             fallback: 'style-loader',
             use: merge([], commonLoaders).concat([{
-                loader: 'stylus-loader',
+                loader: 'stylus-loader'
             }])
-        }),
+        })
     },
     sass: {
         test: /\.s(a|c)ss$/,
         loader: ExtractTextPlugin.extract({
             fallback: 'style-loader', 
             use: merge([], commonLoaders).concat([{
-                loader: 'sass-loader',
+                loader: 'sass-loader'
             }])
-        }),
-    },
+        })
+    }
 };
 
 // 模板loader
@@ -153,11 +153,11 @@ var templateRules = {
     },
     handlebars: { 
         test: /\.handlebars$/, 
-        loader: "handlebars-loader" 
+        loader: 'handlebars-loader' 
     },  
     ejs: {
         test: /\.ejs$/,
-        loader: "ejs-compiled-loader",
+        loader: 'ejs-compiled-loader',
         query: {
             'htmlmin': true, // or enable here  
             'htmlminOptions': {
@@ -170,11 +170,13 @@ var templateRules = {
 configWebpack.style.forEach((style) => {
     style = (style === 'scss') ? 'sass' : style;
     let rule = styleRules[style] || '';
+
     rule && baseConfig.module.rules.push(rule);
 });
 
 configWebpack.template.forEach((tpl) => {
     let rule = templateRules[tpl] || '';
+
     rule && baseConfig.module.rules.push(rule);
 });
 
@@ -182,19 +184,19 @@ let imageLoader = {
     test: /\.(jpe?g|png|gif|svg)$/i,
     loaders: [
         {
-            loader: "url-loader",
+            loader: 'url-loader',
             query: {
                 publicPath: isProduction ? configWebpack.imgCdn : configWebpack.webserver,
                 limit: 1000,
-                name: "img/[path]/" + configWebpack.hashName + ".[ext]",
-            },
-        },
+                name: 'img/[path]/' + configWebpack.hashName + '.[ext]'
+            }
+        }
     ]
 };
 
 baseConfig.module.rules.push(imageLoader);
 
-/************* plugins 处理 *************/
+/** *********** plugins 处理 *************/
 if (isProduction) {
     baseConfig.plugins.push(new webpack.DefinePlugin(configWebpack.injectVar));
     baseConfig.plugins.push(new WebpackMd5Hash());
@@ -204,8 +206,8 @@ if (isProduction) {
             workers: os.cpus().length, // usually having as many workers as cpu cores gives good results 
             // other uglify options 
             compress: {
-                warnings: false,
-            },
+                warnings: true
+            }
         }));
     }
 }
@@ -220,28 +222,27 @@ if (configWebpack.clean) {
 configWebpack.static.forEach((item) => {
     baseConfig.plugins.push(new CopyWebpackPlugin([{
         from: item.src,
-        to: (item.dist || item.src) + (item.hash ? configWebpack.hashName : "[name]") + '.[ext]'
+        to: (item.dist || item.src) + (item.hash ? configWebpack.hashName : '[name]') + '.[ext]'
     }]));
 });
 
-configWebpack.sprites = (configWebpack.spriteMode === "none") ? [] : configWebpack.sprites;
+configWebpack.sprites = (configWebpack.spriteMode === 'none') ? [] : configWebpack.sprites;
 
 configWebpack.sprites.forEach(function(sprites) {
     let style = configWebpack.spriteStyle,
         extMap = {
-            stylus: "styl",
-            less: "less",
-            sass: "sass",
-            scss: "scss"
+            stylus: 'styl',
+            less: 'less',
+            sass: 'sass',
+            scss: 'scss'
         },
-        spriteMode = (!!~sprites.key.indexOf('_retina')) ? "retinaonly" : configWebpack.spriteMode,
+        spriteMode = (~sprites.key.indexOf('_retina')) ? 'retinaonly' : configWebpack.spriteMode,
         retinaTplMap = {
-            retinaonly: "_retinaonly",
-            "normal": "",
-            "retina": "_retina",
+            retinaonly: '_retinaonly',
+            'normal': '',
+            'retina': '_retina'
         },
-        retinaTpl = retinaTplMap[spriteMode] || "";
-
+        retinaTpl = retinaTplMap[spriteMode] || '';
 
     let spritesConfig = {
         src: {
@@ -249,10 +250,10 @@ configWebpack.sprites.forEach(function(sprites) {
             glob: '*.png'
         },
         target: {
-            image: path.join(configWebpack.path.src, "css/sprites/" + sprites.key + ".png"),
+            image: path.join(configWebpack.path.src, 'css/sprites/' + sprites.key + '.png'),
             css: [
                 [
-                    path.join(configWebpack.path.src, "css/sprites/" + sprites.key + "." + extMap[style]),
+                    path.join(configWebpack.path.src, 'css/sprites/' + sprites.key + '.' + extMap[style]),
                     {format: sprites.key}
                 ]
             ]
@@ -261,7 +262,7 @@ configWebpack.sprites.forEach(function(sprites) {
             padding: 10
         },
         apiOptions: {
-            cssImageRef: "~" + sprites.key + ".png"
+            cssImageRef: '~' + sprites.key + '.png'
         }
     };
 
@@ -269,20 +270,20 @@ configWebpack.sprites.forEach(function(sprites) {
         [sprites.key]: path.join(__dirname, '../node_modules/', './spritesheet-templates-steamer/lib/templates/' + style + retinaTpl + '.template.handlebars')
     };
 
-    if (spriteMode === "retina") {
-        spritesConfig.retina = "@2x";
+    if (spriteMode === 'retina') {
+        spritesConfig.retina = '@2x';
     }
 
     baseConfig.plugins.push(new SpritesmithPlugin(spritesConfig));
 });
 
-/************* base 与 user config 合并 *************/
+/** *********** base 与 user config 合并 *************/
 var userConfig = {
     output: configCustom.getOutput(),
     module: configCustom.getModule(),
     resolve: configCustom.getResolve(),
     externals: configCustom.getExternals(),
-    plugins: configCustom.getPlugins(),
+    plugins: configCustom.getPlugins()
 };
 
 var otherConfig = configCustom.getOtherOptions();
