8a335d3eff5b82131649f1f2779ea3315aeaa2d2
773000785@qq.com <773000785@qq.com>
2017-08-02 12:30:34 +0800

    update

--- tools/template/react-mobx/main.js
+++ tools/template/react-mobx/main.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV != 'production'){
     module.exports = require('./root/App.dev.js');
 } else {
-    module.exports = require('./root/App.js');
+    module.exports = require('./root/App.prod.js');
 }
---
+++ tools/template/react-mobx/root/App.prod.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+render(
+    <App/>,
+    document.getElementById('pages')
+)
