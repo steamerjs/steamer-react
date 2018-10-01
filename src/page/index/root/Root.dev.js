d5fa98d3f92f7ff9a09aee8fd50b0ff30bada83d
lcxfs1991 <lcxfs1991@gmail.com>
2018-10-01 17:51:22 +0800

    remove offline-plugin

--- src/page/index/root/Root.dev.js
+++ src/page/index/root/Root.dev.js
@@ -1,4 +1,3 @@
-require('offline-plugin/runtime').install();
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import configureStore from '../stores/configureStore';
